87ec79f893e1d2dc9974189ef481ef779fe44890
Adam Callow <adamjcallow@manx.net>
2021-03-06 23:45:33 +0000

    Add files via upload

---
+++ Messages.js
@@ -0,0 +1,32 @@
+var usermsgs = [
+ {
+   "Username": "AdamCallow",
+   "titleName": "The Real Dispatcher",
+   "bodyMsg": "You live on The Isle of Man? Where's that?",
+ },
+
+ {
+    "Username": "DeerCrusher",
+    "titleName": "#FakePilot",
+    "bodyMsg": "Try not to hit any deer on the way down",
+  },
+
+  {
+    "Username": "tomthetank",
+    "titleName": "Glider Dude",
+    "bodyMsg": "Engines normally stay ON for descent",
+  },
+
+  {
+    "Username": "naro",
+    "titleName": "Better Purdue Guy",
+    "bodyMsg": "We are sorry you have to hang out with Josh that much",
+  },
+
+  {
+    "Username": "Marc",
+    "titleName": "That Swiss Guy",
+    "bodyMsg": "Ok now, thats enough chocolate, you can put the bar down now.",
+  },
+
+]
