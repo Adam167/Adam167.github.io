1573c762972a08bea7fb3922cf061ad3e9fb5a73
Adam Callow <adamjcallow@manx.net>
2021-03-07 17:46:42 +0000

    Add files via upload

--- Messages.js
+++ Messages.js
@@ -19,14 +19,62 @@ var usermsgs = [
 
   {
     "Username": "naro",
-    "titleName": "Better Purdue Guy",
+    "titleName": "better Purdue Guy",
     "bodyMsg": "Sorry you have to hang out with Josh that much",
   },
 
   {
     "Username": "Marc",
-    "titleName": "That Swiss Guy",
+    "titleName": "that Swiss Guy",
     "bodyMsg": "Ok, I think thats enough chocolate now. Put the bar down",
+  },
+
+  {
+    "Username": "MishaCamp",
+    "titleName": "a Unicorn",
+    "bodyMsg": "DIVE, DIVE, DIVE. ARM VNAV NOW, this is not a drill!",
+  },
+
+  {
+    "Username": "Tyler_Shelton",
+    "titleName": "Tyler",
+    "bodyMsg": "Possible pilot deviation. I've got a number for you to call when you land",
+  },
+
+  {
+    "Username": "Drummer",
+    "titleName": "Bruce",
+    "bodyMsg": "",
+  },
+
+  {
+    "Username": "Chris_S",
+    "titleName": "Chris",
+    "bodyMsg": "",
+  },
+
+  {
+    "Username": "Joe",
+    "titleName": "Joe",
+    "bodyMsg": "",
+  },
+
+  {
+    "Username": "JoshFly8",
+    "titleName": "Crashy",
+    "bodyMsg": "You won't make it to the runway, but might as well send a notification",
+  },
+
+  {
+    "Username": "RAH",
+    "titleName": "User Guide Guy",
+    "bodyMsg": "Dump the gear and flaps like your on a 9k base",
+  },
+
+  {
+    "Username": "Cameron",
+    "titleName": "code breaker",
+    "bodyMsg": "Quickly land, then hurry up and fix the code you broke.",
   },
 
 ]
